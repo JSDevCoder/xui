f61de2d37b0e2a384261ab3b94434439f2681651
mnkv <mnkv@163.com>
2019-08-10 17:45:18 +0800

    add: 添加x-button组件

---
+++ xui/components/ui/button.vue
@@ -0,0 +1,245 @@
+<template>
+	<div 
+		class="x-btn" 
+		:class="[computedType, computedSize, computedBtnLine, computedActive, computedBlock]"
+		@tap="btnTap"
+	>
+		{{computedText}}
+	</div>
+</template>
+
+<script>
+	/**
+	 * 按钮组件
+	 */
+	export default {
+		name: 'x-button',
+		props: {
+			// 按钮类型 []
+			btnType: {
+				type: String,
+				default: ''
+			},
+
+			// 按钮大小
+			btnSize: {
+				type: String,
+				default: ''
+			},
+
+			// 按钮文本
+			btnText: {
+				type: String,
+				default: '默认按钮'
+			},
+
+			// 只有边框的按钮
+			btnOutline: {
+				type: String,
+				default: ''
+			},
+
+			// 按钮状态
+			btnActive: {
+				type: String,
+				default: ''
+			},
+
+			// 是否块元素
+			btnBlock: {
+				type: String,
+				default: ''
+			}
+		},
+
+		computed: {
+			computedType() {
+				return this.btnType ? 'x-btn-' + this.btnType : '';
+			},
+
+			computedSize() {
+				return this.btnSize ? 'x-btn-' + this.btnSize : '';
+			},
+
+			computedText() {
+				return this.btnText;
+			},
+
+			computedBtnLine() {
+				return this.btnOutline ? 'x-btn-outline-' + this.btnOutline : '';
+			},
+
+			computedActive() {
+				return this.btnActive === 'true' ? 'x-btn-active' : '';
+			},
+
+			computedBlock() {
+				return this.btnBlock === 'true' ? 'x-btn-block' : '';
+			}
+		},
+		
+		methods: {
+			btnTap() {
+				this.$emit('btnTap');
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	@commonColor: #fff;
+	@primaryColor: #007aff;
+	@secondaryColor: #6c757d;
+	@successColor: #28a745;
+	@dangerColor: #dc3545;
+	@warningColor: #ffc107;
+	@infoColor: #17a2b8;
+	@lightColor: #f8f9fa;
+	@darkColor: #343a40;
+	@linkColor: #007bff;
+	
+	.x-btn {
+		display: inline-block;
+		margin-right:1rem;
+		margin-bottom: 1rem;
+		font-weight: 400;
+		text-align: center;
+		white-space: nowrap;
+		border: 1px solid transparent;
+		font-size: 1rem;
+		padding: .375rem .75rem;
+		line-height: 1.5;
+		border-radius: .25rem;
+		transition: color .15s ease-in-out,
+			background-color .15s ease-in-out,
+			border-color .15s ease-in-out,
+			border-radius .15s ease-in-out;
+
+		&.x-btn-primary {
+			background-color: @primaryColor;
+			color: @commonColor;
+			border-color: @primaryColor;
+		}
+		
+		&.x-btn-outline-primary{
+			background-color: @commonColor;
+			color: @primaryColor;
+			border-color: @primaryColor;
+		}
+
+		&.x-btn-secondary {
+			background-color: @secondaryColor;
+			color: @commonColor;
+			border-color: @secondaryColor;
+		}
+		
+		&.x-btn-outline-secondary{
+			background-color: @commonColor;
+			color: @secondaryColor;
+			border-color: @secondaryColor;
+		}
+		
+		&.x-btn-success{
+			background-color: @successColor;
+			color: @commonColor;
+			border-color: @successColor;
+		}
+		
+		&.x-btn-outline-success{
+			background-color: @commonColor;
+			color: @successColor;
+			border-color: @successColor;
+		}
+		
+		&.x-btn-danger{
+			background-color: @dangerColor;
+			color: @commonColor;
+			border-color: @dangerColor;
+		}
+		
+		&.x-btn-outline-danger{
+			background-color: @commonColor;
+			color: @dangerColor;
+			border-color: @dangerColor;
+		}
+		
+		&.x-btn-warning{
+			background-color: @warningColor;
+			color: @commonColor;
+			border-color: @warningColor;
+		}
+		
+		&.x-btn-outline-warning{
+			background-color: @commonColor;
+			color: @warningColor;
+			border-color: @warningColor;
+		}
+		
+		&.x-btn-info{
+			background-color: @infoColor;
+			color: @commonColor;
+			border-color: @infoColor;
+		}
+		
+		&.x-btn-outline-info{
+			background-color: @commonColor;
+			color: @infoColor;
+			border-color: @infoColor;
+		}
+		
+		&.x-btn-light{
+			background-color: @lightColor;
+			color: @commonColor;
+			border-color: @lightColor;
+		}
+		
+		&.x-btn-outline-light{
+			background-color: @commonColor;
+			color: @lightColor;
+			border-color: @lightColor;
+		}
+		
+		&.x-btn-dark{
+			background-color: @darkColor;
+			color: @commonColor;
+			border-color: @darkColor;
+		}
+		
+		&.x-btn-outline-dark{
+			background-color: @commonColor;
+			color: @darkColor;
+			border-color: @darkColor;
+		}
+		
+		&.x-btn-link{
+			background-color: @linkColor;
+			color: @commonColor;
+			border-color: @linkColor;
+		}
+		
+		&.x-btn-outline-link{
+			background-color: @commonColor;
+			color: @linkColor;
+			border-color: @linkColor;
+		}
+		
+		&.x-btn-lg{
+			padding: .5rem 1rem;
+			font-size: 1.25rem;
+			line-height: 1.5;
+			border-radius: .3rem;
+		}
+		
+		&.x-btn-sm{
+			padding: .25rem .5rem;
+			font-size: .875rem;
+			line-height: 1.5;
+			border-radius: .2rem;
+		}
+		
+		&.x-btn-block{
+			width:100%;
+			display: block;
+		}
+	}
+</style>
--- xui/pages/index/index.vue
+++ xui/pages/index/index.vue
@@ -1,28 +1,54 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="container">
+		<x-button btn-type="primary" btn-text="primary"></x-button>
+		<x-button btn-type="secondary" btn-text="secondary"></x-button>
+		<x-button btn-type="success" btn-text="success"></x-button>
+		<x-button btn-type="danger" btn-text="danger"></x-button>
+		<x-button btn-type="warning" btn-text="warning"></x-button>
+		<x-button btn-type="info" btn-text="info"></x-button>
+		<x-button btn-type="light" btn-text="light"></x-button>
+		<x-button btn-type="dark" btn-text="dark"></x-button>
+		<x-button btn-type="link" btn-text="link"></x-button>
+		<x-button btn-outline="primary" btn-text="primary"></x-button>
+		<x-button btn-outline="secondary" btn-text="secondary"></x-button>
+		<x-button btn-outline="success" btn-text="success"></x-button>
+		<x-button btn-outline="danger" btn-text="danger"></x-button>
+		<x-button btn-outline="warning" btn-text="warning"></x-button>
+		<x-button btn-outline="info" btn-text="info"></x-button>
+		<x-button btn-outline="light" btn-text="light"></x-button>
+		<x-button btn-outline="dark" btn-text="dark"></x-button>
+		<x-button btn-outline="link" btn-text="link"></x-button>
+		<x-button btn-type="primary" btn-size="lg" btn-text="large btn"></x-button>
+		<x-button btn-outline="primary" btn-size="lg" btn-text="大按钮"></x-button>
+		<x-button btn-type="primary" btn-size="sm" btn-text="small btn"></x-button>
+		<x-button btn-outline="primary" btn-size="sm" btn-text="小按钮"></x-button>
+		<x-button btn-type="primary" btn-size="sm" btn-block="true" btn-text="small btn"></x-button>
+		<x-button btn-outline="primary" btn-size="sm" btn-block="true" btn-text="小按钮"></x-button>
 	</view>
 </template>
 
 <script>
 	import service from '../../request/service.js'
+	import xButton from '../../components/ui/button.vue'
 	export default {
 		data() {
 			return {
 				title: 'Hello'
 			}
 		},
+		
+		components: {
+			xButton
+		},
+		
 		onLoad() {
-			service.test('何六', 1, 10).then(res => {
-				if(res.code === 1){
-					
-				}else{
-					service.statusHandle(res.code, res.msg);
-				}
-			});
+			// service.test('何六', 1, 10).then(res => {
+			// 	if(res.code === 1){
+			// 		
+			// 	}else{
+			// 		service.statusHandle(res.code, res.msg);
+			// 	}
+			// });
 		},
 		methods: {
 			
@@ -31,29 +57,7 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	.container{
+		padding:2rem;
 	}
 </style>
